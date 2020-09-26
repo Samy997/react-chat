b42c516f544a9d87bfb278950b8254342ee36a11
Samy997 <zamy.11.4.1997@gmail.com>
2020-09-26 21:34:02 +0200

    Updates

---
+++ precache-manifest.fd94c08108b5683b8a601261102efa13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf5006176fa49930c4422ff2fb08a18",
+    "url": "/react-chat/index.html"
+  },
+  {
+    "revision": "d04d60d2b1f132fbca5b",
+    "url": "/react-chat/static/css/main.380aec51.chunk.css"
+  },
+  {
+    "revision": "b73c4e11384217e64b5d",
+    "url": "/react-chat/static/js/2.95ee85ff.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/react-chat/static/js/2.95ee85ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d04d60d2b1f132fbca5b",
+    "url": "/react-chat/static/js/main.083f0c0e.chunk.js"
+  },
+  {
+    "revision": "9068e11b550cbe5bb855",
+    "url": "/react-chat/static/js/runtime-main.c42a369d.js"
+  }
+]);
